d72a8a82f8f9dd5158345c4cdefa05c95efa5c4e
ErickFlores99 <erickplata133@gmail.com>
2025-05-07 13:20:11 -0600

    Modififcaion de formulario olvido de contraseña

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,36 +1,69 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        <title>Iniciar Sesión</title>
+
+        <!--  Paleta de colores  -->
+        <link rel="stylesheet" href="{{ asset('assets/css/colors.css') }}">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="{{ asset('assets/libs/bootstrap-5.3.3/css/bootstrap.min.css') }}">
+        
+        <!-- FontAwesime Icons  -->
+        <link rel="stylesheet" href="{{ asset('assets/icons/fontawesome-free-6.7.2-web/css/all.min.css') }}">
+    </head>
+    <body>
+        <div class="container-fluid min-vh-100 d-flex align-items-center">
+            <div class="row justify-content-center w-100">
+                <div class="col-12 col-sm-8 col-md-6 col-lg-4">
+                    <div class="card p-4 shadow rounded">
+                        <div class="text-center mb-3">
+                            <a href="{{ route('public_home') }}" class="text-decoration-none text-dark">
+                                <img src="assets/images/logo.webp" alt="Logo" width="48" height="48" class="mb-2">
+                                <h5 class="mb-0">Fut-App</h5>
+                            </a>
+                        </div>
+
+                        <div class="text-center mb-3">
+                            <strong>Iniciar Sesión</strong><br>
+                            <small>
+                                ¿Olvidó su contraseña? No hay problema. Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace de restablecimiento de contraseña que le permitirá elegir uno nuevo.
+                            </small>
+                        </div>
+
+                        <!-- Session Status -->
+                        <x-auth-session-status class="mb-4" :status="session('status')" />
+
+                        <!-- Validation Errors -->
+                        <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                        <form method="POST" action="{{ route('password.email') }}">
+                            @csrf
+                            
+                            <!-- Email Address -->
+                            <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                    <i class="fa-solid fa-at"></i>
+                                </span>
+                                <input class="form-control"
+                                    id="email" 
+                                    type="email" 
+                                    name="email" :value="old('email')" 
+                                    placeholder="Email"
+                                    required autofocus />
+                            </div>
+
+                            <div class="d-grid mb-2">
+                                <button type="submit" class="btn btn-primary">Restablecer</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Session Status -->
-        <x-auth-session-status class="mb-4" :status="session('status')" />
-
-        <!-- Validation Errors -->
-        <x-auth-validation-errors class="mb-4" :errors="$errors" />
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Email Password Reset Link') }}
-                </x-button>
-            </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+    </body>
+</html>
