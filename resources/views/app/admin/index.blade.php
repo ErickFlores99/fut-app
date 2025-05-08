1fba69f1719de5d4fd8a727c0288f0f519baf700
ErickFlores99 <erickplata133@gmail.com>
2025-05-08 09:41:47 -0600

    ejemplos de funcionalidad de tooltips y alerts

--- resources/views/app/admin/index.blade.php
+++ resources/views/app/admin/index.blade.php
@@ -12,6 +12,14 @@
 
 @section('script')
     {{-- TODO: Archivos para los scripts --}}
+    <script>
+        // Alerta SweetAlert2
+        Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"
+            });
+    </script>
 @endsection
 
 <!-- Nav Bar -->
@@ -163,5 +171,39 @@
                 </div>
             </div>
         </div>
+    </div>
+
+    <div>
+        <!-- Tooltip arriba (por defecto) -->
+        <button 
+            data-bs-toggle="tooltip" 
+            title="Arriba (default)" 
+            data-bs-placement="top">
+            Tooltip arriba
+        </button>
+
+        <!-- Tooltip abajo -->
+        <button 
+            data-bs-toggle="tooltip" 
+            title="Tooltip abajo" 
+            data-bs-placement="bottom">
+            Tooltip abajo
+        </button>
+
+        <!-- Tooltip a la izquierda -->
+        <button 
+            data-bs-toggle="tooltip" 
+            title="Tooltip izquierda" 
+            data-bs-placement="left">
+            Tooltip izquierda
+        </button>
+
+        <!-- Tooltip a la derecha -->
+        <button 
+            data-bs-toggle="tooltip" 
+            title="Tooltip derecha" 
+            data-bs-placement="right">
+            Tooltip derecha
+        </button>
     </div>
 @endsection
--- resources/views/app/public/Index.blade.php
+++ resources/views/app/public/Index.blade.php
@@ -15,6 +15,14 @@
 
 @section('script')
     {{-- TODO: Archivos para los scripts --}}
+    <script>
+        // Alerta SweetAlert2
+        Swal.fire({
+                title: "Good job!",
+                text: "You clicked the button!",
+                icon: "success"
+            });
+    </script>
 @endsection
 
 @section('content')
@@ -30,6 +38,40 @@
             <button type="button" class="btn btn-light">Light</button>
             <button type="button" class="btn btn-dark">Dark</button>
             <button type="button" class="btn btn-link">Link</button>
+        </div>
+
+        <div>
+            <!-- Tooltip arriba (por defecto) -->
+            <button 
+                data-bs-toggle="tooltip" 
+                title="Arriba (default)" 
+                data-bs-placement="top">
+                Tooltip arriba
+            </button>
+
+            <!-- Tooltip abajo -->
+            <button 
+                data-bs-toggle="tooltip" 
+                title="Tooltip abajo" 
+                data-bs-placement="bottom">
+                Tooltip abajo
+            </button>
+
+            <!-- Tooltip a la izquierda -->
+            <button 
+                data-bs-toggle="tooltip" 
+                title="Tooltip izquierda" 
+                data-bs-placement="left">
+                Tooltip izquierda
+            </button>
+
+            <!-- Tooltip a la derecha -->
+            <button 
+                data-bs-toggle="tooltip" 
+                title="Tooltip derecha" 
+                data-bs-placement="right">
+                Tooltip derecha
+            </button>
         </div>
     </div>
 @endsection
